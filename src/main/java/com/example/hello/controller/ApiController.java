35ac14d2a920d407a729b4dec250e3c2df590bcb
Daniel <byoung.mokk@gmail.com>
2022-12-20 21:51:33 +0900

    API Controller Update

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -4,12 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // 해당 클래스는 REST API 를 처리하는 Controller
-@RequestMapping("/api/")
+@RestController // 해당 클래스는 REST API 를 처리하는 Controller, @Controller + @RequestBody
+@RequestMapping("/api/") // Base URL
 public class ApiController {
 
     @GetMapping("/hello")   // http://localhost:8080/api/hello
     public String hello() {
+        // 반환타입이 String 이면 view 의 이름이 아닌 문자열 반환
 
         return "hello spring boot!";
 
