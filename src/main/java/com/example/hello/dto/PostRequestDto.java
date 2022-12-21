b7bbd7037af4e507063065342286e86e9b1f39e8
Daniel <byoung.mokk@gmail.com>
2022-12-21 21:31:00 +0900

    POST_API Update

--- src/main/java/com/example/hello/dto/PostRequestDto.java
+++ src/main/java/com/example/hello/dto/PostRequestDto.java
@@ -1,9 +1,14 @@
 package com.example.hello.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
+// 클래스 단위로 묶어 선언된 필드(카멜) -> 스네이크 형식으로 바꿔준다. -> 범위가 넓은 @JsonProperty
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostRequestDto {
     // POST 핸들러 메소드에서 어떤 값을 받을지 모르니 DTO 를 만들어 데이터를 받자
 
