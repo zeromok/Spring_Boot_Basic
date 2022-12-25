eb28f9b9b13e94c7e6174c878bc4e19e595f9665
Daniel <byoung.mokk@gmail.com>
2022-12-25 11:12:17 +0900

    add AOP Comments

--- src/main/java/com/example/hello/aop/ParameterAop.java
+++ src/main/java/com/example/hello/aop/ParameterAop.java
@@ -10,15 +10,18 @@ import org.springframework.stereotype.Component;
 import java.lang.reflect.Method;
 
 @Log4j2
+// AOP 를 정의 하는 클래스
 @Aspect
 @Component
 public class ParameterAop {
 
     @Pointcut("execution(* com.example.hello.controller..*.*(..))")
+    // 기능을 어디에 적용 시킬지?, AOP 를 적용 시킬 지점을 설정
     private void pointCut(){}
 
 
     @Before("pointCut()")
+    // 메소드가 실행되기 이전
     public void before(JoinPoint joinPoint){
 
         // 메소드 이름 가져오기
@@ -37,6 +40,7 @@ public class ParameterAop {
     }
 
     @AfterReturning(value = "pointCut()", returning = "object")
+    // 메소드가 잘 수행되면...
     public void afterReturn(JoinPoint joinPoint, Object object){
         log.info("object : {}", object);
     }
