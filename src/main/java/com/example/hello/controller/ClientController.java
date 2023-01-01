c4f39e80bcd18b3c27f3a88c0336ef384c3eb7fd
Daniel <byoung.mokk@gmail.com>
2023-01-01 13:21:37 +0900

    Feat: Add Client Server
    Server(Client) To Server(Server) 와의 GET 통신

--- src/main/java/com/example/hello/controller/ClientController.java
+++ src/main/java/com/example/hello/controller/ClientController.java
@@ -1,7 +1,8 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.User;
 import com.example.hello.service.RestTemplateService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/client")
 public class ClientController {
 
+//    @Autowired -> 어노테이션을 이용한 주입 (엣버전?)
     // 생성자를 이용한 명시적 주입
     private final RestTemplateService restTemplateService;
 
@@ -18,7 +20,8 @@ public class ClientController {
     }
 
     @GetMapping("/hello")
-    public String getHello() {
+//    public String getHello() {
+    public ResponseEntity<User> getHello() {
         return restTemplateService.hello();
     }
 
--- src/main/java/com/example/hello/service/RestTemplateService.java
+++ src/main/java/com/example/hello/service/RestTemplateService.java
@@ -1,6 +1,8 @@
 package com.example.hello.service;
 
+import com.example.hello.dto.User;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,11 +15,16 @@ public class RestTemplateService {
 
     // http://localhost/api/server/hello
     // response
-    public String hello() {
+//    public String hello() {
+public ResponseEntity<User> hello() {
 
+    // GET 방식
         URI uri = UriComponentsBuilder
                 .fromUriString("http://localhost:9090")
                 .path("/api/server/hello")
+                .queryParam("name", "steve")
+                .queryParam("age", 30)
+//              http://localhost:9090/api/server/hello?name=steve&age=30
                 .encode()
                 .build()
                 .toUri();
@@ -25,7 +32,18 @@ public class RestTemplateService {
         log.info(uri.toString());
 
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
+        // 1. String 으로 받을 때
+//        String result = restTemplate.getForObject(uri, String.class);
+
+        // 2. ResponseEntity 로 받을 때
+//        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+//        log.info("code : {}", result.getStatusCode());
+//        log.info("body : {}", result.getBody());
+
+        // JSON 으로 받으려면?
+        ResponseEntity<User> result = restTemplate.getForEntity(uri, User.class);
+        log.info("code : {}", result.getStatusCode());
+        log.info("body : {}", result.getBody());
 
         return result;
     }
