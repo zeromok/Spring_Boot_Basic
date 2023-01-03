3608a67a759018bcb755672742a997fd285ac27f
zeromok <byoung.mokk@gmail.com>
2023-01-03 22:51:46 +0900

    Feat: Add
    1. AssertTrue/False 와 같은 Method 지정을 통한 Custom Logic 적용
    2. ConstraintValidator 를 적용하여 재사용이 가능한 Custom Logic 적용

--- src/main/java/com/example/hello/dto/ValidUserDTO.java
+++ src/main/java/com/example/hello/dto/ValidUserDTO.java
@@ -1,12 +1,14 @@
 package com.example.hello.dto;
 
+import com.example.hello.annotation.YearMonth;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.format.DateTimeFormatters;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @NoArgsConstructor
@@ -23,6 +25,25 @@ public class ValidUserDTO {
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
     private String phoneNumber;
+
+//    @Size(min = 6, max = 6)
+    @YearMonth
+    private String reqYearMonth;    // yyyyMM
+
+    @AssertTrue(message = "yyyyMM 의 형식에 맞지 않습니다.") // return true 가 되어야 정상, 재활용 불가 하지만 방법이 있음 CustomAnnotation
+    public boolean isReqYearMonthValidation() {
+
+        try{
+
+            // LocalDate 는 기본적으로 yyyyMMdd 이여서 뒤에 01 붙여줌
+            LocalDate localDate = LocalDate.parse(getReqYearMonth() + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+        }catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
 
 
 } // end class
