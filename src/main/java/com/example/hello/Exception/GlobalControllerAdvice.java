7880bf127f0389813566d35cb914995dd323129b
Daniel <byoung.mokk@gmail.com>
2023-01-10 00:07:52 +0900

    test

--- src/main/java/com/example/hello/Exception/GlobalControllerAdvice.java
+++ src/main/java/com/example/hello/Exception/GlobalControllerAdvice.java
@@ -3,7 +3,6 @@ package com.example.hello.Exception;
 import com.example.hello.dto.Error;
 import com.example.hello.dto.ErrorResponse;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
